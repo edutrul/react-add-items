d7b772612bc111699a530055d5d3f7be6fff5773
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2024-07-20 10:14:38 -0500

    Add new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
-function App() {
+function ArtistsList({artists}) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {artists.map( (artist) => <div key={artist.id}>{artist.name}</div>)}
     </div>
   );
 }
 
-export default App;
+function ArtistCreationForm({artists, addArtist}) {
+
+  let [artistName, setArtistName] = useState('');
+
+  const handleChangeArtistName = (e) => setArtistName(e.target.value);
+
+  const handleAddNewArtist = (e) => {
+    e.preventDefault();
+    console.log(e);
+    addArtist(artistName);
+    setArtistName('');
+  }
+
+  return (
+    <form>
+      <input type="textfield" name="artistName" onChange={handleChangeArtistName} value={artistName}/>
+      <button onClick={handleAddNewArtist}>Add new artist</button>
+    </form>
+  );
+}
+
+export default function App() {
+  const [artists, setArtists] = useState([{id: 1, name: 'Axel'}, {id: 2, name:'Ambar'}, {id: 3, name: 'Amy'}]);
+
+  const addArtist = (artistName) => {
+    setArtists( (prevArtists) => [...prevArtists, {id: prevArtists.length + 1, name: artistName}] )
+  }
+
+  return <>
+    <ArtistCreationForm addArtist={addArtist} />
+    <ArtistsList artists={artists}/>
+  </>
+}
